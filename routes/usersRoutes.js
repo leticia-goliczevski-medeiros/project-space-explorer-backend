2adcc50b40e6d11029c1e35ba053a2886835ee9b
leticia-goliczevski-medeiros <leticia_goliczevski@outlook.com>
2025-06-22 18:04:51 -0300

    feat: implement new user routes and new custom error

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { getUser } = require('../controllers/users');
+const { getUser, getUserGallery } = require('../controllers/users');
+const { addCardLike, removeCardLike } = require('../controllers/cards');
 
 const userRouter = express.Router();
 
 userRouter.get('/users/me', getUser);
+userRouter.get('/users/me/gallery', getUserGallery);
+userRouter.post('/users/me/gallery', addCardLike);
+userRouter.delete('/users/me/gallery/:id', removeCardLike);
 
 module.exports = { userRouter };
